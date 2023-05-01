b0dd4eeb57b2e58658505157002766a93d9fd710
hamza <hamza.h96343@gmail.com>
2023-05-01 01:30:34 -0700

    trying_something

--- data.py
+++ data.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import sqlite3
+import psycopg2
+import psycopg2.extras
 
 
-con = sqlite3.connect("messages.db")
+con = psycopg2.connect("postgresql://contact_query_databse_user:SYWt3zrIo3aEjJDqwcxemqaz6HuMMmkU@dpg-ch5l8s5269v5rfrcrp50-a.oregon-postgres.render.com/contact_query_databse")
 df = pd.read_sql_query("SELECT * from Form_Response", con)
 
 
--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3 as sql
+import psycopg2
+import psycopg2.extras
 from datetime import datetime
 import os
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-#postgres://contact_query_databse_user:SYWt3zrIo3aEjJDqwcxemqaz6HuMMmkU@dpg-ch5l8s5269v5rfrcrp50-a.oregon-postgres.render.com/contact_query_databse
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://contact_query_databse_user:SYWt3zrIo3aEjJDqwcxemqaz6HuMMmkU@dpg-ch5l8s5269v5rfrcrp50-a/contact_query_databse"
+
+#postgresql://contact_query_databse_user:SYWt3zrIo3aEjJDqwcxemqaz6HuMMmkU@dpg-ch5l8s5269v5rfrcrp50-a.oregon-postgres.render.com/contact_query_databse
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -16,37 +19,57 @@ app.config['SECRET_KEY'] = 'You cant guess this key'
 
 db = SQLAlchemy(app)
 
-con = sql.connect('messages.db')
-con.execute('CREATE TABLE IF NOT EXISTS Form_Response (ID INTEGER PRIMARY KEY AUTOINCREMENT,'+'name TEXT,email TEXT,subject TEXT,message TEXT,date TEXT)')
+class Response(db.Model):
+    __tablename__ = "Form_Response"
+    name = db.Column(db.String[30])
+    email = db.Column(db.String[30], primary_key=True)
+    subject = db.Column(db.String[40])
+    message = db.Column(db.String[500])
+    data = db.Column(db.Integer)
 
-con.close()
+    def __init__(self, nm, em, sub, mess, da):
+        self.name = nm
+        self.email = em
+        self.subject = sub
+        self.message = mess
+        self.data = da
 
 @app.route('/', methods=["GET", "POST"])
 def portfolio():
     return render_template('index.html')
 
-@app.route('/contact_info', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def contact_info():
+
     email = None
+    # if request.method == "POST":
+    #     email = request.form['email']
+    #     name = request.form['name']
+    #     subject = request.form['subject']
+    #     message = request.form['message']
+    #     date = datetime.now()
+    #     try:
+    #         con = psycopg2.connect('postgresql://contact_query_databse_user:SYWt3zrIo3aEjJDqwcxemqaz6HuMMmkU@dpg-ch5l8s5269v5rfrcrp50-a.oregon-postgres.render.com/contact_query_databse')
+    #         c = con.cursor()
+    #         c.execute("INSERT INTO Form_Response (name,email,subject,message,date) VALUES(?,?,?,?,?)",(name,email,subject,message,date))
+    #         con.commit()
+    #         flash("Thank you for contacting me, I will get back to you as soon as possible")
+    #         render_template('index.html', email=email)
+    #     finally:
+    #         con.close()
     if request.method == "POST":
         email = request.form['email']
         name = request.form['name']
         subject = request.form['subject']
         message = request.form['message']
         date = datetime.now()
-        try:
-            con = sql.connect('messages.db')
-            c = con.cursor()
-            c.execute("INSERT INTO Form_Response (name,email,subject,message,date) VALUES(?,?,?,?,?)",(name,email,subject,message,date))
-            con.commit()
-            flash("Thank you for contacting me, I will get back to you as soon as possible")
-            render_template('index.html', email=email)
-        finally:
-            con.close()
+
+        form_response = Response(name, email, subject, message, date)
+        db.session.add(form_response)
+        db.session.commit()
 
 
     return render_template("index.html")
-
 
 
 
