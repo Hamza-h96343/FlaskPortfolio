d7bd0a63100d90f7dc8608bc12205a45067319f2
hamza <hamza.h96343@gmail.com>
2023-05-01 02:38:01 -0700

    fixed_Databse

--- data.py
+++ data.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import sqlite3
-import psycopg2
-import psycopg2.extras
 
 
-con = psycopg2.connect("postgresql://contact_query_databse_user:SYWt3zrIo3aEjJDqwcxemqaz6HuMMmkU@dpg-ch5l8s5269v5rfrcrp50-a.oregon-postgres.render.com/contact_query_databse")
+con = sqlite3.connect("messages.db")
 df = pd.read_sql_query("SELECT * from Form_Response", con)
 
 
--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://contact_query_databse_user:SYWt3zrIo3aEjJDqwcxemqaz6HuMMmkU@dpg-ch5l8s5269v5rfrcrp50-a/contact_query_databse"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://contact_query_databse_user:SYWt3zrIo3aEjJDqwcxemqaz6HuMMmkU@dpg-ch5l8s5269v5rfrcrp50-a.oregon-postgres.render.com/contact_query_databse"
 
 #postgresql://contact_query_databse_user:SYWt3zrIo3aEjJDqwcxemqaz6HuMMmkU@dpg-ch5l8s5269v5rfrcrp50-a.oregon-postgres.render.com/contact_query_databse
 
@@ -21,48 +21,34 @@ db = SQLAlchemy(app)
 
 class Response(db.Model):
     __tablename__ = "Form_Response"
-    name = db.Column(db.String[30])
-    email = db.Column(db.String[30], primary_key=True)
+    name = db.Column(db.String[30], primary_key=True)
+    email = db.Column(db.String[30])
     subject = db.Column(db.String[40])
     message = db.Column(db.String[500])
-    data = db.Column(db.Integer)
+    date = db.Column(db.TIMESTAMP)
 
     def __init__(self, nm, em, sub, mess, da):
         self.name = nm
         self.email = em
         self.subject = sub
         self.message = mess
-        self.data = da
+        self.date = da
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def portfolio():
     return render_template('index.html')
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["POST"])
 def contact_info():
 
-    email = None
-    # if request.method == "POST":
-    #     email = request.form['email']
-    #     name = request.form['name']
-    #     subject = request.form['subject']
-    #     message = request.form['message']
-    #     date = datetime.now()
-    #     try:
-    #         con = psycopg2.connect('postgresql://contact_query_databse_user:SYWt3zrIo3aEjJDqwcxemqaz6HuMMmkU@dpg-ch5l8s5269v5rfrcrp50-a.oregon-postgres.render.com/contact_query_databse')
-    #         c = con.cursor()
-    #         c.execute("INSERT INTO Form_Response (name,email,subject,message,date) VALUES(?,?,?,?,?)",(name,email,subject,message,date))
-    #         con.commit()
-    #         flash("Thank you for contacting me, I will get back to you as soon as possible")
-    #         render_template('index.html', email=email)
-    #     finally:
-    #         con.close()
     if request.method == "POST":
         email = request.form['email']
         name = request.form['name']
         subject = request.form['subject']
         message = request.form['message']
         date = datetime.now()
+
+        print(email, name, subject, message, date)
 
         form_response = Response(name, email, subject, message, date)
         db.session.add(form_response)
@@ -74,4 +60,6 @@ def contact_info():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+        app.run()
